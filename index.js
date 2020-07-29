40b8eaf920b36248109f9e48585aeafb4aa15fa6
Dan Finlay <dan@danfinlay.com>
2020-07-29 16:24:00 -0700

    Remove comments

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ module.exports = function (xpub, opts = {}) {
     next: () => {
 
       const child =  hdkey.derive(`m/44'/60'/0'/${index}`);
-      console.dir({child: child.toJSON()});
+      // console.dir({child: child.toJSON()});
       const wallet = Wallet.fromPublicKey(child._publicKey);
-      console.log(wallet.getAddress());
+      // console.log(wallet.getAddress());
       index++;
       return wallet.getAddress();
 
